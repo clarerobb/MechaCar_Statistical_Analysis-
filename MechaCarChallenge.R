1fcd57deb82c59fcf52ff555341879e2fdd7e4f7
Clare Robbins <clarerobbins@Clares-MacBook-Pro.local>
2022-09-16 19:18:23 -0500

    Added to RScript for Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,13 @@ mecha_car_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + groun
 
 #Using the summary() function, determine the p-value and r-squared value
 summary(mecha_car_lm)
+
+# Deliverable 2
+#read Suspension_Coil.csv
+suspension <- read.csv('Suspension_Coil.csv', stringsAsFactors = F)
+
+#summary statistics 
+total_summary <- suspension %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+#lot summary
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
